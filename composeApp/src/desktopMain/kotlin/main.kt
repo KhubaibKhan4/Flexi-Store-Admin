ab13d76b939deaa780118f3a645e6329329f6c64
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-06-19 14:13:13 +0500

    -Size Enhancement (#5)

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,10 +1,13 @@
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Flexi-Store-Admin",
+        state = rememberWindowState(width = 1280.dp, height = 720.dp)
     ) {
         App()
     }
