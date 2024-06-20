63a06ca96d9ec71478dd38a678a456c3c7607173
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-06-20 11:20:20 +0500

    -Flexi Client Setup Imp (#41)

--- composeApp/src/commonMain/kotlin/data/remote/FlexiStoreClient.kt
+++ composeApp/src/commonMain/kotlin/data/remote/FlexiStoreClient.kt
@@ -1,7 +1,14 @@
 package data.remote
 
+import domain.model.order.Orders
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
-class FlexiStoreClient(client: HttpClient) {
-
+class FlexiStoreClient(
+    private val client: HttpClient,
+) {
+    suspend fun getAllOrders(): List<Orders> {
+        return client.get("v1/order").body()
+    }
 }
