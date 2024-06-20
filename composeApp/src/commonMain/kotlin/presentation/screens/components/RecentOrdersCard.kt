bba894690fc3ea3eca5a2136b0a4c86ca4128c5c
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-06-20 16:56:51 +0500

    -Order Card Completion (#54)

--- composeApp/src/commonMain/kotlin/presentation/screens/components/RecentOrdersCard.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/components/RecentOrdersCard.kt
@@ -1,13 +1,16 @@
 package presentation.screens.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -15,11 +18,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import domain.model.order.Orders
 import domain.model.products.Products
+import io.kamel.core.Resource
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
+import utils.Constant.BASE_URL
 
 @Composable
 fun RecentOrdersCard(modifier: Modifier, orders: List<Orders>,products: List<Products>) {
@@ -38,19 +48,30 @@ fun RecentOrdersCard(modifier: Modifier, orders: List<Orders>,products: List<Pro
             orders.forEach { order ->
                 val product = products.find { it.id == order.productIds }
                 if (product != null) {
+                    val image : Resource<Painter> = asyncPainterResource(BASE_URL+product.imageUrl)
                     Row(
                         modifier = Modifier.fillMaxWidth(),
                         verticalAlignment = Alignment.CenterVertically
                     ) {
+                        KamelImage(
+                            resource = image,
+                            contentAlignment = Alignment.Center,
+                            contentDescription = "Product Image",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.size(28.dp)
+                                .clip(CircleShape)
+                                .border(width = 1.dp, color = Color.LightGray, CircleShape)
+                        )
+                        Spacer(modifier = Modifier.width(2.dp))
                         Column {
                             Text(text = product.name.take(20), fontSize = 14.sp)
                             Text(text = "#${order.trackingId.take(8)}", fontSize = 12.sp, color = Color.Gray)
                         }
                         Spacer(modifier = Modifier.weight(1f))
-                        Text(text = "\$${order.totalPrice.toString()}", fontSize = 14.sp)
+                        Text(text = "\$${order.totalPrice}", fontSize = 14.sp)
                         Spacer(modifier = Modifier.width(8.dp))
                         Text(
-                            text = if (order.orderProgress=="On Progress") "Order Placed" else if (order.orderProgress == "On The Way") "Out for Delivery" else "Completed",
+                            text = if (order.orderProgress=="On Progress") "Pending" else if (order.orderProgress == "On The Way") "On The Way" else "Completed",
                             fontSize = 12.sp,
                             color = if (order.orderProgress == "Completed") Color.Green else Color.Red,
                             modifier = Modifier
