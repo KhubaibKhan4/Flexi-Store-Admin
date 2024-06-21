3447895830d90947fd5a928ce5c5a4c21eb270c4
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-06-21 09:10:43 +0500

    -Products Init (#58)

--- composeApp/src/commonMain/kotlin/presentation/screens/Products.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/Products.kt
@@ -1,10 +1,101 @@
 package presentation.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.dp
 import domain.model.products.Products
+import io.kamel.core.Resource
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
+import utils.Constant.BASE_URL
 
 @Composable
 fun ProductContent(productList: List<Products>) {
-    Text("Products Screen by Product Content")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF7F7F7))
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Product List",
+            style = MaterialTheme.typography.titleSmall,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        LazyColumn {
+            items(productList) { product ->
+                ProductCard(product)
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun ProductCard(product: Products) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = CardDefaults.cardElevation(4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                val productImages : Resource<Painter> = asyncPainterResource(BASE_URL+product.imageUrl)
+                KamelImage(
+                    resource = productImages,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Column {
+                    Text(text = product.name, style = MaterialTheme.typography.headlineSmall)
+                    Text(text = product.brand, style = MaterialTheme.typography.bodyLarge, color = Color.Gray)
+                    Text(text = "Price: \$${product.price}", style = MaterialTheme.typography.labelLarge, color = Color.Black)
+                }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Button(onClick = { /* TODO: Edit Product */ }) {
+                    Text("Edit")
+                }
+                Button(onClick = { /* TODO: Delete Product */ }, colors = ButtonDefaults.buttonColors(containerColor = Color.Red)) {
+                    Text("Delete")
+                }
+            }
+        }
+    }
 }
