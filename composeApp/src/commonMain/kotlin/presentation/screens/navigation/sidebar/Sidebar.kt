f7c555f7afe3a80e7520a97f098abadec6286a7a
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-06-23 14:19:47 +0500

    -Sidebar Enhancement (#86)

--- composeApp/src/commonMain/kotlin/presentation/screens/navigation/sidebar/Sidebar.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/navigation/sidebar/Sidebar.kt
@@ -16,12 +16,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.ChevronLeft
-import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MenuOpen
 import androidx.compose.material.icons.filled.ShoppingBag
@@ -38,7 +36,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
@@ -56,7 +53,7 @@ fun SidebarMenu(
 ) {
     var isExpanded by remember { mutableStateOf(initialExpandedState) }
     val expandIcon = if (isExpanded) Icons.Default.MenuOpen else Icons.Default.Menu
-    val sidebarWidth by animateDpAsState(targetValue = if (isExpanded) 200.dp else 75.dp)
+    val sidebarWidth by animateDpAsState(targetValue = if (isExpanded) 240.dp else 75.dp)
 
     Column(
         modifier = Modifier
@@ -64,6 +61,9 @@ fun SidebarMenu(
             .fillMaxHeight()
             .background(Color(0xFFf1f4f9))
             .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
