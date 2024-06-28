024804cbc46affcae3a76fab627349f7ce9381ab
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-06-28 12:05:51 +0500

    -Update Profile Impl (#130)

--- composeApp/src/commonMain/kotlin/presentation/screens/profile/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/profile/ProfileScreen.kt
@@ -1,5 +1,8 @@
 package presentation.screens.profile
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,14 +11,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -26,21 +33,30 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import domain.model.users.Users
 import domain.usecase.UiState
+import io.github.vinceglb.filekit.compose.rememberFilePickerLauncher
+import io.github.vinceglb.filekit.core.PickerMode
+import io.github.vinceglb.filekit.core.PickerType
 import io.kamel.core.Resource
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
+import io.ktor.client.statement.HttpResponse
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.jetbrains.skia.Image
 import org.koin.compose.koinInject
 import presentation.screens.components.ErrorScreen
 import presentation.screens.components.LoadingScreen
@@ -52,6 +68,10 @@ class ProfileScreen : Screen {
     override fun Content() {
         val viewModel = koinInject<MainViewModel>()
         var usersData by remember { mutableStateOf<Users?>(null) }
+        var imageBytes by remember { mutableStateOf<ByteArray?>(null) }
+        var loading by remember { mutableStateOf(false) }
+        var responseMessage by remember { mutableStateOf<HttpResponse?>(null) }
+        val scope = rememberCoroutineScope()
 
         LaunchedEffect(Unit) {
             viewModel.getUserById(144)
@@ -74,6 +94,28 @@ class ProfileScreen : Screen {
                 usersData = response
             }
         }
+        LaunchedEffect(imageBytes) {
+            if (imageBytes != null) {
+                viewModel.updateProfileImage(144, imageBytes!!)
+            }
+        }
+        val updateProfileState by viewModel.updateImage.collectAsState()
+        when (updateProfileState) {
+            is UiState.ERROR -> {
+                val error = (updateProfileState as UiState.ERROR).throwable
+                ErrorScreen(error.message.toString(), onRetry = {
+                    if (imageBytes != null) {
+                        viewModel.updateProfileImage(144, imageBytes!!)
+                    }
+                })
+            }
+
+            UiState.LOADING -> {}
+            is UiState.SUCCESS -> {
+                val response = (updateProfileState as UiState.SUCCESS).response
+                responseMessage = response
+            }
+        }
 
         Row(modifier = Modifier.fillMaxSize()) {
             Box(
@@ -93,7 +135,12 @@ class ProfileScreen : Screen {
                             Column(modifier = Modifier.weight(1f).padding(end = 16.dp)) {
                                 ProfileForm(admin)
                             }
-                            ProfileImage(admin.profileImage)
+                            ImageSelection(
+                                imageBytes,
+                                onImageSelected = { imageBytes = it },
+                                imageUrl = usersData?.profileImage.toString(),
+                                responseMessage
+                            )
                         }
                         Spacer(modifier = Modifier.height(16.dp))
                         SaveChangesButton()
@@ -171,27 +218,11 @@ class ProfileScreen : Screen {
         }
     }
 
-    @Composable
-    fun ProfileImage(profileImageUrl: String) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            val image: Resource<Painter> = asyncPainterResource(BASE_URL + profileImageUrl)
-            KamelImage(
-                resource = image,
-                contentDescription = "Profile Image",
-                modifier = Modifier.size(120.dp).clip(RoundedCornerShape(8.dp)),
-                contentScale = ContentScale.Crop
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Button(onClick = {}) {
-                Text("Change Image")
-            }
-        }
-    }
 
     @Composable
     fun SaveChangesButton() {
         ElevatedButton(
-            onClick = {  },
+            onClick = { },
             modifier = Modifier
         ) {
             Text("Save changes")
@@ -215,12 +246,133 @@ class ProfileScreen : Screen {
             Column {
                 Text(text = "Remove account", fontWeight = FontWeight.Bold)
                 ElevatedButton(
-                    onClick = {  },
+                    onClick = { },
                     colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
                 ) {
                     Text("Deactivate")
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ImageSelection(
+    imageBytes: ByteArray?,
+    onImageSelected: (ByteArray) -> Unit,
+    imageUrl: String,
+    responseMessage: HttpResponse?,
+) {
+    var message by remember { mutableStateOf("") }
+    val scope = rememberCoroutineScope()
+    val launcher = rememberFilePickerLauncher(
+        type = PickerType.Image,
+        mode = PickerMode.Single,
+        title = "Pick a Category Image"
+    ) { platformFile ->
+        scope.launch {
+            val bytes = platformFile?.readBytes()
+            if (bytes != null) {
+                onImageSelected(bytes)
+            }
+        }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .wrapContentWidth()
+            .padding(16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .wrapContentWidth()
+                .height(140.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            imageBytes?.let {
+                Image(
+                    bitmap = Image.makeFromEncoded(imageBytes).toComposeImageBitmap(),
+                    contentDescription = "Profile Image",
+                    modifier = Modifier.size(120.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop,
+                )
+            } ?: run {
+                if (imageUrl.isEmpty()) {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .size(120.dp)
+                            .clip(CircleShape)
+                            .clickable { launcher.launch() }
+                    ) {
+                        Text("Select Image", color = Color.Gray)
+                    }
+                } else {
+                    val image: Resource<Painter> = asyncPainterResource(BASE_URL + imageUrl)
+                    KamelImage(
+                        resource = image,
+                        contentDescription = "Profile Image",
+                        modifier = Modifier.size(120.dp).clip(RoundedCornerShape(8.dp)),
+                        contentScale = ContentScale.Crop,
+                    )
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .offset(y = (-12).dp)
+                    .padding(4.dp)
+                    .size(25.dp)
+                    .background(color = Color.LightGray, shape = CircleShape)
+                    .clip(CircleShape)
+                    .align(Alignment.BottomEnd)
+                    .clickable {
+                        launcher.launch()
+                    },
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = null,
+                    modifier = Modifier.size(20.dp)
+                )
+            }
+        }
+        if (responseMessage != null) {
+            if (responseMessage.status.value == 200 ||
+                responseMessage.status.value == 201 ||
+                responseMessage.status.value == 202
+            ) {
+                message = "Profile Image Updated Successfully"
+
+            } else {
+                message = responseMessage.status.description
+            }
+        }
+
+        if (message.isNotEmpty()) {
+            scope.launch {
+                delay(3000)
+                message = ""
+            }
+            Text(
+                text = message,
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Green
+            )
+        } else {
+            scope.launch {
+                delay(3000)
+                message = ""
+            }
+            Text(
+                text = message ?: "",
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Red
+            )
+        }
+
     }
 }
